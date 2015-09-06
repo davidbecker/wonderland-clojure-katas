c10d52589424390a06597ac1304fa37594afef51
dasmau89 <dasmau89@googlemail.com>
2015-09-06 23:02:56 +0200

    decode & encode working

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,31 +1,9 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
-  "encodeme")
-
-(defn decode [keyword message]
-  "decodeme")
-
-(defn decypher [cypher message]
-  "decypherme")
-
-(defn convert "convert ascii code into string" [x]
-  (str(char x)))
-
-(defn substitute [chr offset]
-  (convert
-    (ascii
-      (+
-        (int chr)
-        (- (int offset) 97)))))
-
-(defn rev-substitute [chr offset]
-  (convert
-    (ascii
-      (+
-        (- (int chr) 97)
-        (- 123 (int offset))
-        97))))
+(defn getr [arr offset]
+  (if (get arr offset)
+    (get arr offset)
+    (getr arr (- offset (count arr)))))
 
 (defn ascii "get ascii code from ascii +- offset"
   [a] (if (< a 123)
@@ -34,3 +12,48 @@
           (ascii (+ a 26))
           )
         (ascii (- a 26))))
+
+(defn convert "convert ascii code into string" [x]
+  (str(char x)))
+
+(defn substitute [chr offset]
+  (if (and chr offset)
+    (convert
+      (ascii
+        (+
+          (int chr)
+          (- (int offset) 97))))))
+
+(defn rev-substitute [chr offset]
+  (if (and chr offset)
+    (convert
+      (ascii
+        (+
+          (- (int chr) 97)
+          (- 123 (int offset))
+          97)))))
+
+(defn encode [keyword message]
+    (loop [i 0 m ""]
+      (let [msg (substitute
+        (get (char-array message) i)
+        (getr (char-array keyword) i))]
+      (if (< i (count message))
+        (recur (inc i) (str m msg))
+        (str m)))))
+
+(defn decode [keyword message]
+  (loop [i 0 m ""]
+      (let [msg (rev-substitute
+        (get (char-array message) i)
+        (getr (char-array keyword) i))]
+      (if (< i (count message))
+        (recur (inc i) (str m msg))
+        (str m)))))
+
+(defn decypher [cypher message]
+  "decypherme")
+
+
+
+
