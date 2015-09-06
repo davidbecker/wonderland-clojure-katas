f0b1c243e10b17b8e30c9de94c08d6aaef35d8b8
dasmau89 <dasmau89@googlemail.com>
2015-09-06 19:38:44 +0200

    added helper method for substitution

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -9,3 +9,16 @@
 (defn decypher [cypher message]
   "decypherme")
 
+(defn convert "convert ascii code into string" [x]
+  (str(char x)))
+
+(defn substitute [chr offset]
+  (convert (ascii (+ (int chr)(- (int offset) 97)))))
+
+(defn ascii "get ascii code from ascii +- offset"
+  [a] (if (< a 123)
+        (if (> a 96)
+          a
+          (ascii (+ a 26))
+          )
+        (ascii (- a 26))))
