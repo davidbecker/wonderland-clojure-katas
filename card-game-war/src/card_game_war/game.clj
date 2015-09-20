a192bd0faca382df58c80131cd3b459813eeb6bd
David Becker <dasmau89@googlemail.com>
2015-09-20 22:24:34 +0200

    card game rank test

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,21 @@
         rank ranks]
     [suit rank]))
 
+
+(defn compareIndex [indexRank1 indexRank2]
+	(if (= indexRank1 indexRank2)
+		nil
+		(if (> indexRank1 indexRank2)
+			indexRank1
+			indexRank2)))
+
+(defn higherRank [rank1 rank2]
+	"returs the higher rank. returns nil if the two ranks are the same"
+	(let [index (compareIndex
+				(.indexOf ranks rank1)
+				(.indexOf ranks rank2))]
+		(if index (ranks index))))
+
 (defn play-round [player1-card player2-card])
 
 (defn play-game [player1-cards player2-cards])
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -6,14 +6,29 @@
 ;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
+    (is (= 3 (higherRank 2 3)))
+    (is (= 4 (higherRank 3 4)))
+    (is (= 5 (higherRank 4 5)))
+    (is (= 6 (higherRank 5 6)))
+    (is (= 7 (higherRank 6 7)))
+    (is (= 8 (higherRank 7 8)))
+    (is (= 9 (higherRank 8 9)))
+    (is (= 10 (higherRank 9 10)))
+    (is (= :jack (higherRank 10 :jack))))
+  (testing "queens are higher rank than jacks"
+    (is (= :queen (higherRank :queen :jack))))
+  (testing "kings are higher rank than queens"
+    (is (= :king (higherRank :queen :king))))
+  (testing "aces are higher rank than kings" 
+    (is (= :ace (higherRank :ace :king))))
+  (testing "if the ranks are equal, clubs beat spades"
     (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= 0 1)))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (= 0 1))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
+  (testing "the player loses when they run out of cards"
+    (is (= 0 1))))
 
